66a2c9561bff9df2ec8d9001d9d8764a93025f11
bc-irina <bc-irina@users.noreply.github.com>
2022-06-17 18:52:07 +1000

    webhook-app-report (#1)
    * Online report page

---
+++ Home.cs
@@ -0,0 +1,29 @@
+
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace Company.Function
+{
+    public static class Home
+    {
+        [FunctionName("Home")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "home")] HttpRequest req,ExecutionContext context,
+            ILogger log)
+        {
+            var path = $"{context.FunctionAppDirectory}/report.html";
+            var stream = new FileStream(@path, FileMode.Open);
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(stream);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+    }
+}
